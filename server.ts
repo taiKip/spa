2279d1119390c792ab51e52e21c3bb4261c3db11
tarus <victortarus6@gmail.com>
2024-06-07 19:02:55 +0300

    added image upload

--- server.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-const path = require('path');
-
-const app = express();
-
-app.get('/*', (req, res) => {
-    res.sendFile(path.resolve("frontend","index.html"))
-})
